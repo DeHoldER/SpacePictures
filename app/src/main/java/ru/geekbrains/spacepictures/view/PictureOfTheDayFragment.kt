22471348837e60d6e7429036fc36d9c507096079
MrFox <stanislav-mk@mail.ru>
2021-10-21 01:57:08 +0300

    v0.03 - добавлен BottomSheet с заголовком и описанием

--- app/src/main/java/ru/geekbrains/spacepictures/view/PictureOfTheDayFragment.kt
+++ app/src/main/java/ru/geekbrains/spacepictures/view/PictureOfTheDayFragment.kt
@@ -1,12 +1,17 @@
 package ru.geekbrains.spacepictures.view
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import coil.load
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import ru.geekbrains.spacepictures.model.repository.POD.PictureOfTheDayData
 import ru.geekbrains.spacepictures.PictureOfTheDayViewModel
 import ru.geekbrains.spacepictures.R
@@ -14,12 +19,14 @@ import ru.geekbrains.spacepictures.databinding.FragmentMainBinding
 
 class PictureOfTheDayFragment : Fragment() {
 
+    private val binding get() = _binding!!
+    private var _binding: FragmentMainBinding? = null
+
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
+
     private val viewModel: PictureOfTheDayViewModel by lazy {
         ViewModelProvider(this).get(PictureOfTheDayViewModel::class.java)
     }
-
-    private var _binding: FragmentMainBinding? = null
-    private val binding get() = _binding!!
 
     companion object {
         fun newInstance() = PictureOfTheDayFragment()
@@ -36,9 +43,24 @@ class PictureOfTheDayFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         viewModel.getData().observe(viewLifecycleOwner, { renderData(it) })
+
+        setBottomSheetBehavior(view.findViewById(R.id.bottom_sheet_container))
+        //TODO показать подпись к фото
+
+        binding.inputLayout.setEndIconOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW).apply {
+                data =
+                    Uri.parse("http://ru.wikipedia.org/wiki/${binding.inputEditText.text.toString()}")
+            })
+        }
     }
 
+    private fun setBottomSheetBehavior(bottomSheet: ConstraintLayout) {
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+    }
 
     private fun renderData(data: PictureOfTheDayData) {
         when (data) {
@@ -49,14 +71,19 @@ class PictureOfTheDayFragment : Fragment() {
 //Отобразите ошибку
 //showError("Сообщение, что ссылка пустая")
                 } else {
+                    val header = view?.findViewById<TextView>(R.id.bottom_sheet_description_header)
+                    val description = view?.findViewById<TextView>(R.id.bottom_sheet_description)
+
+                    header?.text = serverResponseData.title
+                    description?.text = serverResponseData.explanation
 //Отобразите фото
 //showSuccess()
 //Coil в работе: достаточно вызвать у нашего ImageView
 //нужную extension-функцию и передать ссылку и заглушки для placeholder
                     binding.imageView.load(url) {
                         lifecycle(this@PictureOfTheDayFragment)
-                        error(R.drawable.ic_twotone_no_photography_24)
-                        placeholder(R.drawable.ic_twotone_no_photography_24)
+                        error(R.drawable.ic_no_photo_vector)
+                        placeholder(R.drawable.ic_no_photo_vector)
                     }
                 }
             }
