4a34eeb42e5d015022787fef8121a88cecfdd59e
MrFox <stanislav-mk@mail.ru>
2021-11-11 02:16:01 +0300

    v6.02 - добавлен макет фрагмента - добавлены кнопки улаления и перетаскивания в заметку

---
+++ app/src/main/java/ru/geekbrains/spacepictures/view/test/NotesFragment.kt
@@ -0,0 +1,9 @@
+package ru.geekbrains.spacepictures.view.test
+
+import ru.geekbrains.spacepictures.databinding.FragmentNotesBinding
+import ru.geekbrains.spacepictures.util.ViewBindingFragment
+
+class NotesFragment : ViewBindingFragment<FragmentNotesBinding>(FragmentNotesBinding::inflate) {
+
+
+}
