d830b3fd55bacaf29c83c6c56e48ddb2220fa185
MrFox <stanislav-mk@mail.ru>
2021-11-13 12:11:05 +0300

    v6.04 - реализовано перетаскивание заметок и удаление через свайп

--- app/src/main/java/ru/geekbrains/spacepictures/view/test/notes/NotesFragment.kt
+++ app/src/main/java/ru/geekbrains/spacepictures/view/test/notes/NotesFragment.kt
@@ -3,24 +3,28 @@ package ru.geekbrains.spacepictures.view.test.notes
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import ru.geekbrains.spacepictures.databinding.FragmentNotesBinding
 import ru.geekbrains.spacepictures.util.ViewBindingFragment
 
 class NotesFragment : ViewBindingFragment<FragmentNotesBinding>(FragmentNotesBinding::inflate) {
 
+    lateinit var itemTouchHelper: ItemTouchHelper
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val noteData = arrayListOf(
             NoteData("Earth"),
             NoteData("Earth"),
-            NoteData("Mars", "Марс марс марс"),
+            NoteData("Mars", ""),
             NoteData("Earth"),
             NoteData("Earth"),
             NoteData("Earth"),
             NoteData("Mars", null)
         )
-        binding.recyclerView.adapter = NotesAdapter(
+
+        val adapter = NotesAdapter(
             object : NotesAdapter.OnListItemClickListener {
                 override fun onItemClick(NoteData: NoteData) {
                     Toast.makeText(
@@ -29,8 +33,19 @@ class NotesFragment : ViewBindingFragment<FragmentNotesBinding>(FragmentNotesBin
                     ).show()
                 }
             },
-            noteData
+            noteData,
+            object : NotesAdapter.OnStartDragListener {
+                override fun onStartDrag(viewHolder: RecyclerView.ViewHolder) {
+                    itemTouchHelper.startDrag(viewHolder)
+                }
+            }
         )
+
+        binding.recyclerView.adapter = adapter
+        binding.recyclerFragmentFAB.setOnClickListener { adapter.appendItem() }
+
+        itemTouchHelper = ItemTouchHelper(ItemTouchHelperCallback(adapter))
+        itemTouchHelper.attachToRecyclerView(binding.recyclerView)
     }
 
 
